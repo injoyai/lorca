0680cf863d29f0ebef360397d8ae0f82d04742ff
钱纯净 <1113655791@qq.com>
2023-05-24 22:30:04 +0800

    增加easy.go快速创建

---
+++ easy.go
@@ -0,0 +1,59 @@
+package lorca
+
+import (
+	"io/ioutil"
+	"net/url"
+	"os"
+	"os/signal"
+	"strings"
+)
+
+type Config struct {
+	Width   int //宽
+	Height  int //高
+	Html    string
+	Dir     string
+	Options []string
+}
+
+func (this *Config) new() *Config {
+	if this.Width == 0 {
+		this.Width = 480
+	}
+	if this.Height == 0 {
+		this.Height = 320
+	}
+	if strings.HasPrefix(this.Html, "./") || strings.HasPrefix(this.Html, "/") {
+		bs, err := ioutil.ReadFile(this.Html)
+		if err == nil {
+			this.Html = string(bs)
+		}
+	}
+	if len(this.Html) == 0 {
+		this.Html = `
+	<html>
+		<head><title>Hello</title></head>
+		<body><h1>Hello, world!</h1></body>
+	</html>`
+	}
+	return this
+}
+
+func Run(cfg *Config, fn func(UI) error) error {
+	cfg.new()
+	ui, err := New("data:text/html,"+url.PathEscape(cfg.Html), cfg.Dir, cfg.Width, cfg.Height, cfg.Options...)
+	if err != nil {
+		return err
+	}
+	defer ui.Close()
+	if err = fn(ui); err != nil {
+		return err
+	}
+	sign := make(chan os.Signal)
+	signal.Notify(sign, os.Interrupt)
+	select {
+	case <-sign:
+	case <-ui.Done():
+	}
+	return nil
+}
